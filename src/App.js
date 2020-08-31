3fee51a3a5fe7e29e2141fa897fbdd145bc63879
Alexey <runavenison@gmail.com>
2020-09-01 02:18:35 +0300

    les 47 brgin

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import { Route } from "react-router-dom";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import News from "./components/News/News";
 import DialogsContainer from './components/Dialogs/DialogsContainer';
+import NavbarContainer from './components/Navbar/NavbarContainer';
 
 
 const App = (props) => {
     return (
         <div className='app-wrapper'>
             <Header />
-            <Navbar store={props.state} />
+            <NavbarContainer store={props.store} />
             <div className='app-wrapper__content'>
                 <Route path='/profile'
                     render={() => <Profile store={props.store} />} />
--- src/redux/sitebar-reducer.js
+++ src/redux/sitebar-reducer.js
@@ -1,13 +1,13 @@
 
-   let initialState = {
-      friends: [
-         { name: 'Andrew', ava: 'https://cdnimg.rg.ru/img/content/132/44/35/vliianie_kofe_d_850.jpg' },
-         { name: 'Alina', ava: 'https://bigpicture.ru/wp-content/uploads/2019/04/grandbeauty00.jpg' },
-         { name: 'Dima', ava: 'https://i.lb.ua/093/06/5eafcd0924b16.jpeg' }
-      ]
-   };
-   
-   const sitebarReducer = (state = initialState, action) => {
+let initialState = {
+   friends: [
+      { name: 'Andrew', ava: 'https://cdnimg.rg.ru/img/content/132/44/35/vliianie_kofe_d_850.jpg' },
+      { name: 'Alina', ava: 'https://bigpicture.ru/wp-content/uploads/2019/04/grandbeauty00.jpg' },
+      { name: 'Dima', ava: 'https://i.lb.ua/093/06/5eafcd0924b16.jpeg' }
+   ]
+};
+
+const sitebarReducer = (state = initialState, action) => {
    return state;
 };
 
